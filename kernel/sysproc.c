9c8667d01a5be9927a13ee4723e0f475808d74ca
smlee <smlee>
2024-04-24 17:03:24 +0800

    Update user/usys.pl (which generates user/usys.S), kernel/syscall.h, and kernel/syscall.c to allow alarmtest to invoke the sigalarm and sigreturn system calls. For now, your sys_sigreturn should just return zero.

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -96,3 +96,17 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_sigalarm(void)
+{
+   printf("sigalarm says hi\n");
+   return 0;
+}
+
+uint64
+sys_sigreturn(void)
+{
+   printf("sigalarm says hi\n");
+   return 0;
+}
